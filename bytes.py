ae414c25a07eb29e7e5f6793ff07c85a75b277a1
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-21 19:58:42 +0530

    Add files via upload

---
+++ bytes.py
@@ -0,0 +1,28 @@
+# y=b"python"
+# print(y,type(y))
+# print(list(y))
+# print(tuple(y))
+# print(set(y))
+
+# y=b"python"
+# for i in y:
+#     print(i)
+
+# print(ord('p'))
+# print(chr(80))
+
+# y=b"python"
+# y[0]=80 # no modifications
+
+# v=b'[1,2,3]'
+# print(list(v))
+
+# print(ord('['))
+# print(ord(','))
+# print(chr(91))
+
+# m=bytearray(b"python")
+# print(m)
+# print(m[0])
+# m[0]=88
+# print(m)
